ead47aa54cdf215c11d7bd5670f471d1580eb07a
paujim <paujim@hotmail.com>
2020-12-02 06:11:20 +1100

    update tunnel

--- pkg/tunnel/tunnel.go
+++ pkg/tunnel/tunnel.go
@@ -5,23 +5,29 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
-	"sync"
 	"time"
 
 	"golang.org/x/crypto/ssh"
 )
 
+// TunelIface ..
 type TunelIface interface {
-	Start() error
+	Start()
 	Stop()
+	Error() error
 }
 
+// Tunnel ..
 type Tunnel struct {
-	localEndpoint   *Endpoint
-	proxyEndpoint   *Endpoint
-	remoteEndpoint  *Endpoint
-	sshConfig       *ssh.ClientConfig
-	closeConnection chan bool
+	localEndpoint  *Endpoint
+	proxyEndpoint  *Endpoint
+	remoteEndpoint *Endpoint
+	sshConfig      *ssh.ClientConfig
+	err            error
+	conns          []net.Conn
+	proxyConns     []*ssh.Client
+	close          chan interface{}
+	errCn          chan error
 }
 
 func publicKeyFile(file string) ssh.AuthMethod {
@@ -29,7 +35,6 @@ func publicKeyFile(file string) ssh.AuthMethod {
 	if err != nil {
 		return nil
 	}
-
 	key, err := ssh.ParsePrivateKey(buffer)
 	if err != nil {
 		return nil
@@ -37,6 +42,99 @@ func publicKeyFile(file string) ssh.AuthMethod {
 	return ssh.PublicKeys(key)
 }
 
+func (t *Tunnel) forward(localConn net.Conn) {
+	proxyConn, err := ssh.Dial("tcp", t.proxyEndpoint.String(), t.sshConfig)
+	if err != nil {
+		log.Printf("Error connecting to proxy: %s\n", err)
+		t.errCn <- err
+		return
+	}
+	log.Printf("connected to %s (1 of 2)\n", t.proxyEndpoint.String())
+	remoteConn, err := proxyConn.Dial("tcp", t.remoteEndpoint.String())
+	if err != nil {
+		log.Printf("Error connecting to remote: %s\n", err)
+		t.errCn <- err
+		return
+	}
+	t.conns = append(t.conns, remoteConn)
+	t.proxyConns = append(t.proxyConns, proxyConn)
+	log.Printf("connected to %s (2 of 2)\n", t.remoteEndpoint.String())
+	copyConn := func(writer, reader net.Conn) {
+		_, err := io.Copy(writer, reader)
+		if err != nil {
+			log.Printf("io.Copy error: %s", err)
+		}
+	}
+	go copyConn(localConn, remoteConn)
+	go copyConn(remoteConn, localConn)
+
+	return
+}
+
+// Start ..
+func (t *Tunnel) Start() {
+	log.Println("Start Tunnel")
+	listener, err := net.Listen("tcp", t.localEndpoint.String())
+	if err != nil {
+		log.Println("Unable to create local connection")
+		return
+	}
+	defer listener.Close()
+
+	log.Println("Listening for connections")
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Printf("Error accepting connections: %s\n", err)
+		return
+	}
+	t.conns = append(t.conns, conn)
+	log.Println("Accepted connection")
+	go t.forward(conn)
+	select {
+	case <-t.close:
+		log.Println("Closing Signal")
+	case err = <-t.errCn:
+		t.err = err
+		log.Println("Error Signal")
+	}
+
+	var total int
+	total = len(t.conns)
+	for i, conn := range t.conns {
+		log.Printf("Closing [net.Conn] connections (%d of %d)", i+1, total)
+		err := conn.Close()
+		if err != nil {
+			log.Printf(err.Error())
+		}
+	}
+	total = len(t.proxyConns)
+	for i, conn := range t.proxyConns {
+		log.Printf("Closing [ssh.Client] connections (%d of %d)", i+1, total)
+		err := conn.Close()
+		if err != nil {
+			log.Printf(err.Error())
+		}
+	}
+
+	log.Printf("Tunnel closed")
+	return
+}
+
+// Stop ..
+func (t *Tunnel) Stop() {
+	log.Println("Stop Tunnel")
+	if t.err == nil {
+		t.close <- struct{}{}
+	}
+	return
+}
+
+// Error ..
+func (t *Tunnel) Error() error {
+	return t.err
+}
+
+// CreateTunnel ..
 func CreateTunnel(local *Endpoint, proxy *Endpoint, proxyUser, proxyKeyFile string, remote *Endpoint) TunelIface {
 	return &Tunnel{
 		localEndpoint:  local,
@@ -52,61 +150,7 @@ func CreateTunnel(local *Endpoint, proxy *Endpoint, proxyUser, proxyKeyFile stri
 			},
 			Timeout: 3 * time.Second,
 		},
-		closeConnection: make(chan bool),
+		close: make(chan interface{}),
+		errCn: make(chan error),
 	}
-}
-
-func (t *Tunnel) Start() error {
-	log.Printf("Tunnel started!\n")
-	listener, err := net.Listen("tcp", t.localEndpoint.String())
-	if err != nil {
-		log.Printf("Unable to create local connection: %s\n", err)
-		return err
-	}
-	defer listener.Close()
-
-	conn, err := listener.Accept()
-	if err != nil {
-		log.Printf("Unbale to accept listener: %s\n", err)
-		return err
-	}
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go t.forward(conn, t.sshConfig, &wg)
-	wg.Wait()
-	return nil
-}
-
-func (t *Tunnel) Stop() {
-	log.Printf("Tunnel stoped!\n")
-	t.closeConnection <- true
-}
-
-func (t *Tunnel) forward(localConn net.Conn, sshConfig *ssh.ClientConfig, wg *sync.WaitGroup) {
-	proxyConn, err := ssh.Dial("tcp", t.proxyEndpoint.String(), sshConfig)
-	if err != nil {
-		log.Printf("Unable to open proxy connection: %s\n", err)
-		return
-	}
-
-	remoteConn, err := proxyConn.Dial("tcp", t.remoteEndpoint.String())
-	if err != nil {
-		log.Printf("Unable to open remote connection: %s\n", err)
-		return
-	}
-
-	copyConn := func(writer, reader net.Conn) {
-		_, err := io.Copy(writer, reader)
-		if err != nil {
-			log.Printf("Error coping: %s\n", err)
-		}
-	}
-
-	go copyConn(localConn, remoteConn)
-	go copyConn(remoteConn, localConn)
-	<-t.closeConnection
-	localConn.Close()
-	proxyConn.Close()
-	remoteConn.Close()
-	wg.Done()
 }
